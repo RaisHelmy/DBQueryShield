d71746c5f654d1d4f672dd21b29c413fffc5ad3f
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-08-07 16:20:31 +0800

    + Update multi Param using ForEach + Adding [Produces("App/json")] to controller //return only Json

--- Controllers/DBQueryController.cs
+++ Controllers/DBQueryController.cs
@@ -1,29 +1,31 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace DBQuery.Controllers
 {
-
     //[Authorize]
     [ApiController]
     [Route("[controller]")]
-
+    [Produces("application/json")]
     public class DBQueryController : Controller
     {
         [HttpGet]
-        //public async Task<IActionResult> GetAsync([FromHeader] string Name, [FromHeader] string Password, string Query)
         public async Task<IActionResult> GetAsync(string Query)
         {
-            //use Name
-            //Console.WriteLine($"Receiving Userinfo: {Name} Querying for {Query}");
+            var servicesParam = new string[] { "jpjic", "jpn", "jpj", "orangdkhd", "oranghilang", "kenderaanhilang", "personal", "saman", "jim" };
+            string? dbqueryresult = null;
             Console.WriteLine($"Receiving Userinfo: UserTest Querying for {Query}");
             var url = "http://10.32.8.204:3000";
-            using var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("servicesParam", "jpjic, jpn, jpj, orangdkhd, oranghilang, kenderaanhilang, personal, saman, jim");
-            client.DefaultRequestHeaders.Add("searchParam", $"{Query}");
-            var res = await client.GetStringAsync(url);
-            Console.WriteLine(res);
-            return Ok(res);
+            foreach (var param in servicesParam)
+            {
+                using var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("servicesParam", param);
+                client.DefaultRequestHeaders.Add("searchParam", $"{Query}");
+                var res = await client.GetStringAsync(url);
+                dbqueryresult += res;
+            }
+            return Ok(dbqueryresult);
         }
     }
 }
